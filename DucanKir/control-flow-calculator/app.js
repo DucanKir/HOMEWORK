70ce9d1ca0c047685fdae3c363c4ddcd706d33ca
Lana Kirilenko <svetlana.kirilenko@hotmail.com>
2019-07-01 17:33:14 +0100

    basic calculator with loop

--- DucanKir/control-flow-calculator/app.js
+++ DucanKir/control-flow-calculator/app.js
@@ -1,25 +1,29 @@
 // Ask if he wants to display
-// let userWantsToPlay = true
+
 // wich operations
 alert("Let's do some math!")
 
-var operation = prompt('Choose operation (-, +, /, *)')
-// 1st number
-var first = Number(prompt('Choose first number'))
-// 2nd number
-var second = Number(prompt('Choose second number'))
+let userWantsToPlay = true
+while(userWantsToPlay) {
 
-if(operation === '+') {
-  var result = first + second
-}else if (operation === '-') {
-  result = first - second
-}else if (operation === '/') {
-  result = first / second
-} else {
-  result = first * second
+  var operation = prompt('Choose operation (-, +, /, *)')
+  // 1st number
+  var first = Number(prompt('Choose first number'))
+  // 2nd number
+  var second = Number(prompt('Choose second number'))
+
+  if(operation === '+') {
+    var result = first + second
+  }else if (operation === '-') {
+    result = first - second
+  }else if (operation === '/') {
+    result = first / second
+  } else {
+    result = first * second
+  }
+
+  // results
+  alert(first+operation+second+'='+result)
+  // try again
+  userWantsToPlay = confirm('Would you like to try again?')
 }
-
-// results
-alert(result)
-// try again
-// userWantsToPlay = confirm('Would you like to try again?')
