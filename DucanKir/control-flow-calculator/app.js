cfd92140eb283550e075f4485c89c9ff62be8ed4
Lana Kirilenko <svetlana.kirilenko@hotmail.com>
2019-07-01 18:15:27 +0100

    power and square root

--- DucanKir/control-flow-calculator/app.js
+++ DucanKir/control-flow-calculator/app.js
@@ -7,23 +7,34 @@ let userWantsToPlay = true
 while(userWantsToPlay) {
 
   var operation = prompt('Choose operation (-, +, /, *)')
-  // 1st number
-  var first = Number(prompt('Choose first number'))
-  // 2nd number
-  var second = Number(prompt('Choose second number'))
 
-  if(operation === '+') {
-    var result = first + second
-  }else if (operation === '-') {
-    result = first - second
-  }else if (operation === '/') {
-    result = first / second
-  } else {
-    result = first * second
+  if(operation === 'square root') {
+    var squareRoot = Number(prompt('Choose number'))
+    result = Math.sqrt(squareRoot)
+    alert(result)
+    userWantsToPlay = confirm('Would you like to try again?')
   }
+  // 1st number
+  else {
+    var first = Number(prompt('Choose first number'))
+    // 2nd number
+    var second = Number(prompt('Choose second number'))
 
-  // results
-  alert(first+operation+second+'='+result)
-  // try again
-  userWantsToPlay = confirm('Would you like to try again?')
+    if(operation === '+') {
+      var result = first + second
+    }else if (operation === '-') {
+      result = first - second
+    }else if (operation === '/') {
+      result = first / second
+    }else if (operation === 'power') {
+      result = Math.pow(first, second)
+    }else {
+      result = first * second
+    }
+
+    // results
+    alert(first+operation+second+'='+result)
+    // try again
+    userWantsToPlay = confirm('Would you like to try again?')
+  }
 }
