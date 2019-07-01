1f0349f822e14f45f4721a21d405397f0222efc8
Lana Kirilenko <svetlana.kirilenko@hotmail.com>
2019-07-01 18:49:53 +0100

    false answers

--- DucanKir/control-flow-calculator/app.js
+++ DucanKir/control-flow-calculator/app.js
@@ -6,34 +6,51 @@ alert("Let's do some math!")
 let userWantsToPlay = true
 while(userWantsToPlay) {
 
-  var operation = prompt('Choose operation (-, +, /, *)')
+  var operation = prompt('Choose operation: (A)addition, (S)substraction, (D)division, (M)multiplication, (R)square root or (P)power')
 
-  if(operation === 'square root') {
-    var squareRoot = Number(prompt('Choose number'))
+  if(operation === 'R') {
+    var squareRoot = Number(prompt('Choose a number'))
+    while(isNaN(squareRoot) === true) {
+      squareRoot = Number(prompt('Not a number.Choose a number'))
+      if(isNaN(squareRoot) === false) {
+        break
+      }
+    }
     result = Math.sqrt(squareRoot)
     alert(result)
     userWantsToPlay = confirm('Would you like to try again?')
   }
   // 1st number
   else {
-    var first = Number(prompt('Choose first number'))
+    var first = Number(prompt('Choose a first number'))
+    while(isNaN(first) === true) {
+      first = Number(prompt('Not a number.Choose a number'))
+      if(isNaN(first) === false) {
+        break
+      }
+    }
     // 2nd number
-    var second = Number(prompt('Choose second number'))
-
-    if(operation === '+') {
+    var second = Number(prompt('Choose a second number'))
+    while(isNaN(second) === true) {
+      second = Number(prompt('Not a number.Choose a number'))
+      if(isNaN(second) === false) {
+        break
+      }
+    }
+    if(operation === 'A') {
       var result = first + second
-    }else if (operation === '-') {
+    }else if (operation === 'S') {
       result = first - second
-    }else if (operation === '/') {
+    }else if (operation === 'D') {
       result = first / second
-    }else if (operation === 'power') {
+    }else if (operation === 'P') {
       result = Math.pow(first, second)
-    }else {
+    }else if (operation === 'M'){
       result = first * second
     }
 
     // results
-    alert(first+operation+second+'='+result)
+    alert('The result is '+result)
     // try again
     userWantsToPlay = confirm('Would you like to try again?')
   }
