006e49f475145bb7c63488e272a9962c18778d3e
Lana Kirilenko <svetlana.kirilenko@hotmail.com>
2019-07-04 17:50:26 +0100

    finished

---
+++ DucanKir/object-class-problems/problems.js
@@ -0,0 +1,156 @@
+/* eslint-disable no-unused-vars */
+
+/*
+Create an object that defines a laptop. It should have the following properties:
+
+make (string, eg: 'Apple')
+model (string, eg: 'MacBook Pro')
+ram (number, eg: 16)
+storage (number, eg: 250)
+*/
+
+const laptop = {
+  make: 'Apple',
+  model: 'MacBook Pro',
+  ram: 16,
+  storage: 250
+}
+
+/*
+Create an object that defines a bottle of wine. It should have the following properties:
+
+name (string)
+grape (string, eg: Merlot)
+vintage (number, eg: 2017)
+volume (number, eg: 750)
+amountRemaining (number, eg: 750)
+
+It should have the following methods:
+
+drink(amount) - removes the `amount` from the `amountRemaining`
+refill(amount) - adds the `amount` to the amountRemaining
+
+*/
+
+const wineBottle = {
+  name: 'Smth',
+  grape: 'Merlot',
+  vintage: 2017,
+  volume: 750,
+  amountRemaining: 750,
+  drink(amount){
+    return this.amountRemaining -= amount
+  },
+  refill(amount) {
+    return this.amountRemaining += amount
+  }
+}
+
+/*
+Write a Product class that has the following properties:
+
+name (string)
+description (string)
+price (number)
+*/
+
+class Product {
+  constructor(name, description, price) {
+    this.name = name
+    this.description = description
+    this.price = price
+  }
+}
+
+/*
+Write a Cart class that has the following properties:
+
+contents (array)
+
+and the following methods:
+
+addItem(product) - adds the given product into the contents array
+removeItem(product) - removes the given product from the contents array
+empty() - removes all items from the contents array
+getTotal() - returns the total price of all items in the contents array
+
+The product would be an instance of the Product class created above
+*/
+
+class Cart {
+  constructor() {
+    this.contents = []
+  }
+  addItem (item) {
+    return this.contents.push(item)
+  }
+  removeItem (item) {
+    this.contents = this.contents.filter((thing) => thing !== item)
+  }
+  empty () {
+    this.contents = []
+  }
+  getTotal () {
+    return this.contents.reduce((total, item) => total + item.price, 0)
+  }
+}
+
+/*
+Write a Shape class that has the following properties:
+
+width (number)
+height (number)
+
+and the following methods:
+
+getArea() - calculates the area of the shape
+getPerimeter() - calculates the total length of all sides of the shape
+*/
+
+class Shape {
+  constructor(width, height) {
+    this.width = width
+    this.height = height
+
+  }
+  getArea () {
+    return this.width * this.height
+  }
+  getPerimeter () {
+    return (this.width*2)+(this.height*2)
+  }
+}
+
+/*
+Write a Square class that extends the Shape class above. It should have the same properties and methods as the Shape class, but only take a width when being instantiated. The height should be automatically set to be equal to the width.
+*/
+
+class Square extends Shape{
+  constructor(width) {
+    super(width, width)
+  }
+}
+
+/*
+Write a Tringle class that extends the Shape class above. It should have the following properties:
+
+width (number)
+height (number)
+
+and the following methods:
+
+getArea() - calculates the area of the triangle
+getPerimeter() - calculates the perimeter of the triangle (assuming it's right-angled, so width + height + √width + √height)
+*/
+
+class Triangle extends Shape {
+  constructor(width, height) {
+    super (width, height)
+  }
+  getArea(){
+    return (this.width*this.height)/2
+  }
+  getPerimeter () {
+    return this.height+this.width+Math.sqrt(this.height)+ Math.sqrt(this.width)
+  }
+}
