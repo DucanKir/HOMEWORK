2f2e2e33a9c39220e3c02330067480b8fd6c58f2
Lana Kirilenko <svetlana.kirilenko@hotmail.com>
2019-07-10 23:46:14 +0100

    game with style and effects

--- DucanKir/whack-a-mole/app.js
+++ DucanKir/whack-a-mole/app.js
@@ -4,9 +4,12 @@ const startButton = document.querySelector('.start')
 const gameCells = document.querySelectorAll('img')
 const resetButton = document.querySelector('.reset')
 const scoreCounter = document.querySelector('.score')
+const scoreModal = document.querySelector('.scoreModal')
 const timer = document.querySelector('.timer')
-var timerId
+const modal = document.querySelector('.modal')
+
 let gameId
+var timerId
 var timerTime = 30
 let score = 0
 scoreCounter.innerHTML = score
@@ -16,18 +19,20 @@ const img = 'img/mole-hole-png-3.png'
 console.log(gameCells)
 
 function startGame () {
-  setInterval(function(){
+  timerId = setInterval(function(){
     if (timerTime === 0) {
       clearInterval(timerId)
       clearInterval(gameId)
+      modal.style.display = 'flex'
     } else {
       timerTime--
       timer.textContent = timerTime
     }
   }, 1000)
-  let gameId = setInterval(function(){
+  gameId = setInterval(function(){
     gamePlay()
   }, 1000)
+  startButton.disabled = true
 
 }
 
@@ -39,28 +44,36 @@ function gamePlay () {
   }, 1000)
 }
 
-// function cellInteraction () {
-//   if(gameCells.src === 'img/mole-hole-png-3.png') {
-//     score++
-//   }
-// }
 startButton.addEventListener('click', startGame)
 
 gameCells.forEach( function (element) {
   element.addEventListener('click', function () {
     score++
     scoreCounter.innerHTML = score
+    scoreModal.innerHTML = score
+    scoreChange(scoreCounter)
     console.log(score)
+    console.log(timerId)
+    console.log(gameId)
+
   })
 })
 
 resetButton.addEventListener('click', function (){
+  clearInterval(timerId)
+  clearInterval(gameId)
+  startButton.disabled = false
   score = 0
   scoreCounter.innerHTML = score
   timerTime = 30
-  timer.innetHTML = 30
-  gameId = null
-  timerId = null
-  clearInterval(timerId)
-  clearInterval(gameId)
+  timer.innerHTML = 30
+  modal.style.display = 'none'
+  scoreModal.innerHTML = score
 })
+
+function scoreChange (score) {
+  score.classList.add('scoreChange')
+  setTimeout(function(){
+    score.classList.remove('scoreChange')
+  }, 500)
+}
